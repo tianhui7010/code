f4e2e71358a1dffa0f0c2d20c534441ae1dadd32
tianhui <526136300@qq.com>
2019-08-26 13:06:29 +0800

    add

---
+++ haijun/src/main/java/com/tian/haijun/HaijunApplication.java
@@ -0,0 +1,13 @@
+package com.tian.haijun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HaijunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HaijunApplication.class, args);
+	}
+
+}
---
+++ haijun/src/test/java/com/tian/haijun/HaijunApplicationTests.java
@@ -0,0 +1,16 @@
+package com.tian.haijun;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HaijunApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}

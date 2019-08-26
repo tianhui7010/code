7b99bab69e6ac612432a32d755d8c209a6c9a7f5
tianhui <526136300@qq.com>
2019-08-26 13:19:37 +0800

    hahaha

--- jian-15/src/main/java/com/tian/jianZaiJi/Jian15Application.java
+++ jian-15/src/main/java/com/tian/jianZaiJi/Jian15Application.java
@@ -6,7 +6,7 @@ import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
-//测试提交jian-15 b2
+//测试提交jian-15 b2222
 public class Jian15Application {
 
 	public static void main(String[] args) {

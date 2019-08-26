bedd9551b1a3747dd152f6bebd4eb8f34c7bd670
tianhui <526136300@qq.com>
2019-08-26 13:16:45 +0800

    add

--- haijun/src/main/java/com/tian/haijun/HaijunApplication.java
+++ haijun/src/main/java/com/tian/haijun/HaijunApplication.java
@@ -3,7 +3,7 @@ package com.tian.haijun;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//测试提交 haijun b
+//测试提交 haijun b2
 @SpringBootApplication
 public class HaijunApplication {
 
